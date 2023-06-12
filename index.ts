2e6d4ee27dcb97b51f089fa93783a1807f48c6d2
ΔO <david@deltazero.cz>
2023-06-12 21:23:23 +0200

    upgrade to libxmljs 1

--- index.ts
+++ index.ts
@@ -1,17 +1,26 @@
-import { parseXmlString, ValidationError, ParserOptions } from "libxmljs"
+import { parseXml } from "libxmljs"
+import type { XMLParseOptions } from "libxmljs"
 
 // todo: bin linking
 
 export default function validateSchema(
     xml: string|Buffer,
     xsdSchema: string|Buffer,
-    xmlParserOptions ?: ParserOptions,
-    xsdParserOptions ?: ParserOptions
+    xmlParserOptions ?: XMLParseOptions,
+    xsdParserOptions ?: XMLParseOptions
 ) : true|ValidationError[] {
-  const parsedXML = parseXmlString(xml.toString(), xmlParserOptions),
-        parsedSchema = parseXmlString(xsdSchema.toString(), xsdParserOptions)
+  const parsedXML = parseXml(xml.toString(), xmlParserOptions),
+        parsedSchema = parseXml(xsdSchema.toString(), xsdParserOptions)
 
+  // @ts-ignore
   return parsedXML.validate(parsedSchema) || parsedXML.validationErrors
 }
 
-export { ValidationError }
+export interface ValidationError extends Error {
+  domain: number|null;
+  code: number|null;
+  level: number|null;
+
+  line: number|null;
+  column: number;
+}
